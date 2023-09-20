25705f5c140183352cccae70282d2e4a726a0fd4
Emma Linares <misswatson@MacBook-Air-de-Emma.local>
2023-09-19 22:40:20 -0300

    cambios 09-19

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+document.addEventListener ("DOMContentLoaded",
+funtion (){
+    const resetbutton = document.getElementById ("resetbutton");
+    resetbutton.addEventListener("click", Analizar);
+
+});
+
+funtion userinput(){
+    const userinput =document.getElementsByName ("userinput").value;
+}
