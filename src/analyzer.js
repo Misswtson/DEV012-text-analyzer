200dc5f6e7cc85f478d53aa40bd1959dffa617f3
Emma Linares <elinares235@gmail.com>
2023-09-27 10:34:32 -0300

    new-update

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,23 +1,41 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  if (text === "" || (!text.match(/[a-zA-Z0-7]/))) {
+   return 0; 
+  } else {
+    const palabras = text.split(/\s+/);
+    return palabras.length;
+  }
+ 
+},
   getCharacterCount: (text) => {
-    return text.length;
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  if (text === "") {
+    return 0; 
+  } else {
+  return text.length;
+}
+   
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    if (text === "" || (!text.match(/[a-zA-Z0-7]/))) {
+      return 0; 
+     } else {
+      const espacios = text.trim().split(/\s+/);
+      return espacios.length;
+     }
+    
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getAverageWordLength: (text) => { 
+    
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    
+  
   },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  getNumberSum: (text) => {   
+   
+
+  }
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
-//import analyzer from './analyzer.js';
+import analyzer from './analyzer.js';
 
-function limpiarInput() {
-    const inputElement = document.querySelector('[name="user-input"]');// Obtener el elemento de entrada por su ID
-    inputElement.value = ""; // Establecer el valor del campo de entrada en una cadena vacía
-}
-
+const inputElement = document.querySelector('[name="user-input"]');
 const limpiarButton = document.getElementById("reset-button");
-limpiarButton.addEventListener("click", limpiarInput);
+const wordCount = document.querySelector('li.result:nth-child(1)');
+const charCount = document.querySelector('li.result:nth-child(2)');
+const noSpaces = document.querySelector('li.result:nth-child(3)');
+const wordLenght = document.querySelector('li.result:nth-child(4)');
+const numCount = document.querySelector('li.result:nth-child(5)');
+const sumCount = document.querySelector('li.result:nth-child(6)');
 
 
-/*
-document.addEventListener ("DOMContentLoaded"),
-funtion (){
-    const resetButton = document.getElementById("resetbutton");
-    resetbutton.addEventListener("click", Analizar);
 
-});
+limpiarButton.addEventListener("click", function (){
+    inputElement.value = "";
+    wordCount.textContent = "Palabras: 0";
+    charCount.textContent = "Caracteres: 0";
+    noSpaces.textContent = "Espacios: 0";
+    wordLenght.textContent = "Longitud: 0";
+    numCount.textContent = "Números 0";
+    sumCount.textContent = "Sumatoria: 0";
+    console.log ("Se han limpiado los valores");
+    alert("Se han limpiado los valores " + "!");
+})
 
-function userinput(){
-    const userinput =document.getElementsByName ("userinput").value;
-}
-*/
+inputElement.addEventListener("input", function (){
+    const text = inputElement.value
+    wordCount.textContent = "Palabras: " + analyzer.getWordCount(text);
+    charCount.textContent = "Caracteres: " + analyzer.getCharacterCount(text);
+    noSpaces.textContent = "Espacios: " + analyzer.getCharacterCountExcludingSpaces(text);
+    wordLenght.textContent = "Longitud: " + analyzer.getAverageWordLength(text); 
+    numCount.textContent = "Números: " + analyzer.getNumberCount(text);
+    sumCount.textContent = "Sumatoria: " + analyzer.getNumberSum(text);
+    
+    
+})
+
+wordCount.textContent = "Palabras: 0";
+charCount.textContent = "Caracteres: 0";
+noSpaces.textContent = "Espacios: 0";
+wordLenght.textContent = "Longitud: 0";
+numCount.textContent = "Números 0";
+sumCount.textContent = "Sumatoria: 0";
