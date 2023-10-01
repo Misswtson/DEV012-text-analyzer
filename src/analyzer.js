27a2630bd625970ee89c8b557f5b6a739fda6c99
Emma Linares <elinares235@gmail.com>
2023-10-01 19:05:22 -0300

    Modificaciones nuevas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,69 +1,54 @@
 const analyzer = {  
-  getWordCount: (text) => {
-  if (text === "" || (!text.match(/[a-zA-Z0-9]/))) {
-   return 0; 
-  } else {
-    const palabras = text.split(/\s+/);
-    return palabras.length;
-  }
- 
-},
-  getCharacterCount: (text) => {
-  if (text === "") {
-    return 0; 
-  } else {
-  return text.length;
-}
-   
-  },
-  getCharacterCountExcludingSpaces: (text) => {
-    if (text === "" || (!text.match(/[a-zA-Z0-9]/))) {
-      return 0; 
-     } else {
-      const espacios = text.trim().split(/\s+/);
-      return espacios.length;
-     }
-    
-  },
-  getAverageWordLength: (text) => { 
-   const palabrasEncontradas =  text.match(/\w+/g); 
- if (palabrasEncontradas && palabrasEncontradas.length > 0) {
-    let sumaLongitudes = 0;
-    // Utilizar un bucle for...of para sumar las longitudes de las palabras
-    for (const palabra of palabrasEncontradas) {
-        sumaLongitudes += palabra.length;
-    }
-    // Calcular la longitud media como un número de punto flotante (float)
-    const longitudMedia = parseFloat(sumaLongitudes) / palabrasEncontradas.length;
-    return parseFloat(longitudMedia.toFixed(2)); 
+getWordCount: (text) => {
+if (text === "" || (!text.match(/[a-zA-Z0-9]/))) {
+return 0; 
 } else {
-  return 0;
+const palabra = text.split(/\s+/);
+return palabra.length;
 }
-   
-  },
-
-  getNumberCount: (text) => {
-    const numeros = text.match(/\b\d+(\.\d+)?\b/g)
-    if (numeros === null) {
-      return 0;
-    } else {
-      return numeros.length;
-
-    }
-  },
-
-  getNumberSum: (text) => {   
-    const sumaNumeros = text.match(/\b\d+(\.\d+)?\b/g);
-    let suma = 0;
-    if (sumaNumeros === null) {
-      return 0;
-    }
-    for (let i = 0; i < sumaNumeros.length; i++) {
-      suma += parseFloat(sumaNumeros[i]);
-    }
-    return suma;
-    
-  },
+}, 
+getCharacterCount: (text) => {
+if (text === "") {
+return 0; 
+} else {
+return text.length;
+}
+},
+getCharacterCountExcludingSpaces: (text) => {
+let espacios = text.replace(/[^\w]/g, '');
+return espacios.length; 
+},
+getAverageWordLength: (text) => { 
+const palabras = text.split(/\s+/);
+if (palabras && palabras.length > 0) {
+let longitudPalabras = 0;
+for (const palabra of palabras) {
+longitudPalabras += palabra.length;
+}
+const longitud = longitudPalabras / palabras.length;
+console.log(`Longitud media de palabras: ${palabras}`)
+return parseFloat(longitud.toFixed(2));
+}
+},
+getNumberCount: (text) => {
+const numeros = text.match(/\b\d+(\.\d+)?\b/g)
+if (numeros === null) {
+return 0;
+} else {
+return numeros.length;
+}
+},
+getNumberSum: (text) => {   
+const sumaNumeros = text.match(/\b\d+(\.\d+)?\b/g);
+let suma = 0;
+if (sumaNumeros === null) {
+return 0;
+}
+for (let i = 0; i < sumaNumeros.length; i++) {
+suma += parseFloat(sumaNumeros[i]);
+}
+return suma;  
+},
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -9,32 +9,27 @@ const wordLenght = document.querySelector('li.result:nth-child(4)');
 const numCount = document.querySelector('li.result:nth-child(5)');
 const sumCount = document.querySelector('li.result:nth-child(6)');
 
-
-
 limpiarButton.addEventListener("click", function (){
-    inputElement.value = "";
-    wordCount.textContent = "Palabras: 0";
-    charCount.textContent = "Caracteres: 0";
-    noSpaces.textContent = "Espacios: 0";
-    wordLenght.textContent = "Longitud: 0";
-    numCount.textContent = "Números 0";
-    sumCount.textContent = "Sumatoria: 0";
-    console.log ("Se han limpiado los valores");
-    
+inputElement.value = "";
+wordCount.textContent = "Palabras: 0";
+charCount.textContent = "Caracteres: 0";
+noSpaces.textContent = "Espacios: 0";
+wordLenght.textContent = "Longitud: 0";
+numCount.textContent = "Números 0";
+sumCount.textContent = "Sumatoria: 0";
+console.log ("Se han limpiado los valores");   
 })
 
 inputElement.addEventListener("input", function (){
-    const text = inputElement.value
-    wordCount.textContent = "Palabras: " + analyzer.getWordCount(text);
-    charCount.textContent = "Caracteres: " + analyzer.getCharacterCount(text);
-    noSpaces.textContent = "Espacios: " + analyzer.getCharacterCountExcludingSpaces(text);
-    wordLenght.textContent = "Longitud: " + analyzer.getAverageWordLength(text); 
-    numCount.textContent = "Números: " + analyzer.getNumberCount(text);
-    sumCount.textContent = "Sumatoria: " + analyzer.getNumberSum(text);
-    
-    
+const text = inputElement.value
+wordCount.textContent = "Palabras: " + analyzer.getWordCount(text);
+charCount.textContent = "Caracteres: " + analyzer.getCharacterCount(text);
+noSpaces.textContent = "Espacios: " + analyzer.getCharacterCountExcludingSpaces(text);
+wordLenght.textContent = "Longitud: " + analyzer.getAverageWordLength(text); 
+numCount.textContent = "Números: " + analyzer.getNumberCount(text);
+sumCount.textContent = "Sumatoria: " + analyzer.getNumberSum(text);  
 })
-
+//Inicio de mis metricas en 0
 wordCount.textContent = "Palabras: 0";
 charCount.textContent = "Caracteres: 0";
 noSpaces.textContent = "Espacios: 0";
