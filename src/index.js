813ff83e64cbcb22ebfa65a788614e0dd400ad09
Emma Linares <elinares235@gmail.com>
2023-10-03 14:25:33 -0300

    Cambios Finales-Ahora si lo lograste

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import analyzer from './analyzer.js';
 
 const inputElement = document.querySelector('[name="user-input"]');
 const limpiarButton = document.getElementById("reset-button");
-const wordCount = document.querySelector('li.result:nth-child(1)');
-const charCount = document.querySelector('li.result:nth-child(2)');
-const noSpaces = document.querySelector('li.result:nth-child(3)');
-const wordLenght = document.querySelector('li.result:nth-child(4)');
-const numCount = document.querySelector('li.result:nth-child(5)');
-const sumCount = document.querySelector('li.result:nth-child(6)');
+const wordCount = document.querySelector('li[data-testid="word-count"]');
+const charCount = document.querySelector('li[data-testid="char-count"]');
+const noSpaces = document.querySelector('li[data-testid="char-no-spaces-count"]');
+const wordLenght = document.querySelector('li[data-testid="agv-word-length"]');
+const numCount = document.querySelector('li[data-testid="num-count"]');
+const sumCount = document.querySelector('li[data-testid="num-sum"]');
 
 //Inicio de mis metricas en cero, lo modifique porque no las estaba carganado a tiempo. 
-/*wordCount.textContent = "Palabras: 0";
+wordCount.textContent = "Palabras: 0";
 charCount.textContent = "Caracteres: 0";
 noSpaces.textContent = "Espacios: 0";
 wordLenght.textContent = "Longitud: 0";
 numCount.textContent = "Números 0";
-sumCount.textContent = "Sumatoria: 0";*/
+sumCount.textContent = "Sumatoria: 0";
 
 limpiarButton.addEventListener("click", function (){
 inputElement.value = "";
